ed56c776f22c7b50c774d1dcce467cb765490d44
krishnadabhi <dabhikrishna755@gmail.com>
2024-06-19 10:32:00 +0530

    GH-43 changes

--- includes/class-email-details.php
+++ includes/class-email-details.php
@@ -1,32 +1,27 @@
 <?php
-
 namespace CustomkmMenu\Includes;
-
-// Define constant for plugin directory path if not already defined
-if ( ! defined( 'CUSTOMKM_MENU_PLUGIN_DIR' ) ) {
-	define( 'CUSTOMKM_MENU_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-}
 
 /**
  * Class Email_Details
  * Handles functionality related to displaying sent email details.
- * @package CustomkmMenu
- * @subpackage Includes
+ *
+ * @package CustomkmMenu\Includes
  * @since 1.0.0
  */
 class Email_Details {
 
-	/*
+	/**
 	 * Constructor.
 	 */
 	public function __construct() {
-		add_shortcode( 'sent_emails', array( $this, 'customkm_display_sent_emails' ) );
+		add_shortcode( 'customkm_sent_emails', array( $this, 'customkm_display_sent_emails' ) );
 	}
 
 	/**
-	* Retrieves sent emails and outputs them in a list.
-	* @return string Output HTML for displaying sent email details.
-	*/
+	 * Retrieves sent emails and outputs them in a list.
+	 *
+	 * @return string Output HTML for displaying sent email details.
+	 */
 	public function customkm_display_sent_emails() {
 		$sent_emails = get_posts(
 			array(
@@ -42,9 +37,8 @@ class Email_Details {
 			$output .= '<ul>';
 			foreach ( $sent_emails as $email ) {
 				$email_address = get_post_meta( $email->ID, 'email', true );
-				$output .= '</ul>';
-				$sent_time     = get_post_meta( $email->ID, 'mail', true ); // Assuming 'mail' is the meta key for storing sent time
-				$output       .= '<li>Email: ' . $email_address . ' - Sent Time: ' . $sent_time . '</li>';
+				$sent_time     = get_post_meta( $email->ID, 'mail', true );
+				$output .= '<li>Email: ' . $email_address . ' - Sent Time: ' . $sent_time . '</li>';
 			}
 			$output .= '</ul>';
 		} else {
